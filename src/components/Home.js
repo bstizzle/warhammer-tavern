0031e7ee8db3ad60de0ea1c2dd6a1b97d9d3e3ca
Ben Stein <whereyouben@gmail.com>
2021-06-09 14:42:15 -0400

    lambda test button

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Col, Typography } from 'antd';
 const { Title } = Typography;
 
 const Home = () => {
+  const [loading, setLoading] = useState(false)
+
+  const handleClick = api => e => {
+    e.preventDefault()
+
+    this.setState({ loading: true })
+    fetch("/.netlify/functions/" + api)
+      .then(response => response.json())
+      .then(json => {
+        console.log(json)
+        setLoading(false)
+      })
+  }
 
   return(
     <Col>
       <Title>HOME PAGE</Title>
+      <button onClick={handleClick("characters")}>{loading ? "Loading..." : "Get characters"}</button>
     </Col>
   );
 }
