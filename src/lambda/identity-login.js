7cf53c7f7f4a5f543710bc76087eccd8df7b32d6
Ben Stein <whereyouben@gmail.com>
2021-06-25 21:43:52 -0400

    identity-login test

--- src/lambda/identity-login.js
+++ src/lambda/identity-login.js
@@ -1,12 +1,12 @@
 exports.handler = async function(event, context) {
   const { identity, user } = context.clientContext;
 
+  console.log('test output')
   console.log(identity, user)
   return {
     statusCode: 200,
     body: JSON.stringify({
-      identity,
-      user
+      "app_metadata": { roles: ["member"]}
     })
   }
 }
