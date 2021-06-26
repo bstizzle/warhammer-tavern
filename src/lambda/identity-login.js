828e065065f365e821833651af0ac2c2034e2e35
Ben Stein <whereyouben@gmail.com>
2021-06-25 21:47:55 -0400

    same

--- src/lambda/identity-login.js
+++ src/lambda/identity-login.js
@@ -2,7 +2,8 @@ exports.handler = async function(event, context) {
   const { identity, user } = context.clientContext;
 
   console.log('test output')
-  console.log(identity, user)
+  console.log('identity', identity)
+  console.log('user', user)
   return {
     statusCode: 200,
     body: JSON.stringify({
