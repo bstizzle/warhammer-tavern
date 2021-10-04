78f693da49d2d4577acc0c2771d52f2c610e9b49
Ben Stein <whereyouben@gmail.com>
2021-10-04 15:09:04 -0400

    character list filters by logged in email

--- src/pages/CharacterList.js
+++ src/pages/CharacterList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
+import netlifyIdentity from "netlify-identity-widget";
 import gql from 'graphql-tag';
 import { useQuery } from '@apollo/react-hooks';
 import Loading from '../components/Loading';
@@ -15,6 +16,7 @@ const { Title, Text } = Typography;
 const GET_CHARACTERS = gql`
   {
     characters {
+      userId
       _id
       bio {
         name
@@ -28,13 +30,10 @@ const GET_CHARACTERS = gql`
 const CharacterList = () => {
   const history = useHistory();
   const { loading, error, data } = useQuery(GET_CHARACTERS)
+  const user = netlifyIdentity.currentUser();
 
   if (loading) return <Loading />;
   if (error) return <p>Error :(</p>;
-
-  if(data){
-    console.log(data)
-  }
 
   const handleRoute = (e, id) => {
     e.preventDefault();
@@ -44,24 +43,28 @@ const CharacterList = () => {
   return(
     <Row gutter={20}>
       {data.characters.map(c => {
-        return(
-          <Col key={c._id} span={6}>
-            <Card
-              style={{alignItems: 'center'}}
-              actions={[
-                <span onClick={(e) => handleRoute(e, c._id)}>View Sheet</span>,
-                <span>Edit</span>,
-                <span>Delete</span>
-              ]}
-            >
-              <Meta
-                avatar={<Avatar size={60} src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
-                title={<Title level={4}>{c.bio.name}</Title>}
-                description={<Text>{c.bio.species} / {c.bio.careerLevel}</Text>}
-              />
-            </Card>
-          </Col>
-        )
+        if(c.userId === user.email){
+          return(
+            <Col key={c._id} span={6}>
+              <Card
+                style={{alignItems: 'center'}}
+                actions={[
+                  <span onClick={(e) => handleRoute(e, c._id)}>View Sheet</span>,
+                  <span>Edit</span>,
+                  <span>Delete</span>
+                ]}
+              >
+                <Meta
+                  avatar={<Avatar size={60} src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+                  title={<Title level={4}>{c.bio.name}</Title>}
+                  description={<Text>{c.bio.species} / {c.bio.careerLevel}</Text>}
+                />
+              </Card>
+            </Col>
+          );
+        } else {
+          return null;
+        }
       })}
     </Row>
   )
