21d451fbee49bcb5c99062f022edeae0882bfc7b
Ben Stein <whereyouben@gmail.com>
2021-10-04 22:48:02 -0400

    removed yellow errors

--- src/components/bio-components/Movement.js
+++ src/components/bio-components/Movement.js
@@ -1,12 +1,13 @@
 import React, { useState, useContext, useEffect } from 'react';
 import { CharContext } from '../CharContextProvider';
-import { Descriptions, InputNumber, Button, Typography } from 'antd';
+import { Descriptions, Popover, InputNumber, Button, Typography } from 'antd';
 const { Text } = Typography;
 
 const Movement = () => {
   const { char, setChar } = useContext(CharContext);
   const move = char.movement;
   const [moveState, setMoveState] = useState(move);
+  const [visible, setVisible] = useState(false)
 
   useEffect(() => {
     console.log('move effect!')
@@ -15,6 +16,14 @@ const Movement = () => {
       move: moveState
     }))
   }, [setChar, moveState])
+  
+  function handleVisible(){
+    if(visible === false){
+      setVisible(true)
+    } else {
+      setVisible(false)
+    }
+  }
 
   return(
     <Descriptions
@@ -25,11 +34,30 @@ const Movement = () => {
       column={{ xxl: 3, xl: 3, lg: 2, md: 2, sm: 1, xs: 1 }}
     >
       <Descriptions.Item key="movement" label="Movement">
-        <Button>
-          <Text>
-            Move: {moveState} | Walk: {moveState*2} | Run: {moveState*4}
-          </Text>
-        </Button>
+        <Popover
+          content={
+            <>
+            <span>Move:</span> <InputNumber
+              style={{maxWidth: '60px'}}
+              size="small"
+              min={0}
+              max={10}
+              defaultValue={moveState}
+              onChange={setMoveState}
+            />
+            </>
+          }
+          title='Movement'
+          trigger="click"
+          visible={visible}
+          onVisibleChange={handleVisible}
+        >
+          <Button>
+            <Text>
+              Move: {moveState} | Walk: {moveState*2} | Run: {moveState*4}
+            </Text>
+          </Button>
+        </Popover>
       </Descriptions.Item>
     </Descriptions>
   );
